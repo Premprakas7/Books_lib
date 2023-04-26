4b4d53229f9ad7cdd7870bb13c22dee31b879179
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-04-26 19:26:30 +0530

    updated

--- frontend/src/Pages/Display.jsx
+++ frontend/src/Pages/Display.jsx
@@ -4,17 +4,20 @@ import React, { useEffect, useState } from 'react'
 const Display = () => {
     const [data,setData]=useState();
     const [loading, setLoading]=useState(false);
-    const [error, setError]=useState(false)
 
     useEffect(()=>{
         setLoading(true)
         axios.get("http://localhost:5000/books")
-        .then((res)=>setData(res.data))
-        .catch((err)=>setError(err.error))
+        .then((res)=>setData(res.data), setLoading(false))
+        .catch((err)=>console.log(err))
     },[])
     console.log(data);
   return (
     <div>
+        {loading && <div>...loading</div>}
+        {
+            
+        }
       
     </div>
   )
