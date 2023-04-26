222e0f98fc3a2257080e1e6b03653af808816fc9
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-04-26 16:55:22 +0530

    getting data

--- frontend/src/Pages/Display.jsx
+++ frontend/src/Pages/Display.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const Display = () => {
+    const [data,setData]=useState();
+    const [loading, setLoading]=useState(false);
+    const [error, setError]=useState(false)
+
+    useEffect(()=>{
+        setLoading(true)
+        axios.get("http://localhost:5000/books")
+        .then((res)=>setData(res.data))
+        .catch((err)=>setError(err.error))
+    },[])
+    console.log(data);
   return (
     <div>
       
